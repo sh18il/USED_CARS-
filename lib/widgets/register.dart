8564c86e1cb824bb408ef3473cceeac037b54800
sh18il <sshibil379@gmail.com>
2024-02-16 22:47:43 -0800

    update

--- lib/widgets/register.dart
+++ lib/widgets/register.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:used_caer/widgets/bottombar.dart';
 
 import 'sign_in.dart';
 
